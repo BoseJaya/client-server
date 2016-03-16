be904904cd5b0cb7241c92f9bd9f9757e8fc58e5
BoseJaya <sweet.jayabose@gmail.com>
2016-03-17 00:24:39 +0530

    It is very useful....
    hit ipconfig on cmd and get your lapi's ip address and there you go.....

--- src/ClientTest.java
+++ src/ClientTest.java
@@ -3,7 +3,7 @@ import javax.swing.JFrame;
 public class ClientTest {
 	public static void main(String[] args) {
 		Client charlie;
-		charlie = new Client("127.0.0.1");
+		charlie = new Client("Your laptop ip ");
 		charlie.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
 		charlie.startRunning();
 	}
